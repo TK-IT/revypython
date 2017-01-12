dd670fc1ef5af68ade70a745a08f00bd55082415
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2017-01-12 16:58:29 +0100

    Add list_scenes.py helper program

---
+++ lists/list_scenes.py
@@ -0,0 +1,14 @@
+import os
+
+
+def main():
+    for d in sorted(os.listdir()):
+        if d.lower() in ('sange', 'sketches', 'fisk'):
+            for f in sorted(os.listdir(d), key=str.lower):
+                base, ext = os.path.splitext(f)
+                if ext == '.tex':
+                    print("\\input{%s/%s}" % (d, base))
+
+
+if __name__ == '__main__':
+    main()
